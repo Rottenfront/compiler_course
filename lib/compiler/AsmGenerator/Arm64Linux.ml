2f6648544a7ee2fd9fccde23a4d2a62628718499
Andre Rottenfront <andrey.i.bankov@gmail.com>
2025-10-21 08:04:21 +0300

    Disabled variable storage registers, they should be stored and restored into the stack on call

--- lib/compiler/AsmGenerator/Arm64Linux.ml
+++ lib/compiler/AsmGenerator/Arm64Linux.ml
@@ -5,10 +5,13 @@ open Utils
 
 let align16 n = (n + 15) / 16 * 16
 let add_line acc s = acc := !acc ^ s ^ "\n"
-let buffer_reg = "x16"
-let second_buffer_reg = "x20"
-let additional_buffer_reg = "x21"
-let temp_regs = [ "x9"; "x10"; "x11"; "x12"; "x13"; "x14"; "x15" ]
+let buffer_reg = "x9"
+let second_buffer_reg = "x10"
+let additional_buffer_reg = "x11"
+
+(* let temp_regs =
+  [ "x19"; "x20"; "x21"; "x22"; "x23"; "x24"; "x25"; "x26"; "x27"; "x28" ] *)
+let temp_regs = []
 let input_regs = [ "x0"; "x1"; "x2"; "x3"; "x4"; "x5"; "x6"; "x7" ]
 let stack_position sp_shift index = sp_shift - (index * 8)
 
